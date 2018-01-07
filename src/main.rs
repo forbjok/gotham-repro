8485a961610a7e25afa80b8fba9be504b30c09fb
Forb.Jok <forbjok@gmail.com>
2018-01-07 15:13:11 +0100

    cannot infer an appropriate lifetime due to conflicting requirements

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,32 @@
+#![feature(proc_macro, conservative_impl_trait, generators)]
+
+extern crate futures_await as futures;
 extern crate gotham;
 #[macro_use] extern crate gotham_derive;
 extern crate hyper;
-#[macro_use] extern crate slog;
+#[macro_use] extern crate log;
+extern crate serde;
+#[macro_use] extern crate serde_json;
+
+use futures::prelude::*;
+use futures::stream::*;
+use hyper::{Body, Response, StatusCode};
+use gotham::handler::{HandlerError, HandlerFuture, IntoHandlerError};
+use gotham::state::{State, FromState};
 
 fn main() {
 }
 
-#[derive(StateData, PathExtractor, StaticResponseExtender)]
-struct GetThreadRequestPath {
-    no: i64,
+#[async(boxed)]
+fn json_body<T : serde::de::DeserializeOwned>(state: &mut State) -> Result<T, hyper::Error> {
+    use std::io::*;
+
+    let res = {
+        let body = Body::take_from(state);
+        let valid_body = await!(body.concat2())?;
+
+        serde_json::from_reader(valid_body.to_vec().as_slice()).unwrap()
+    };
+
+    Ok(res)
 }
